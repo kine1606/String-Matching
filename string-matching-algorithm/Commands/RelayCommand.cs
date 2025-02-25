443df4a52d0900105aa71dabc36b85147a8db795
Superkid <kevinchristensen76923@gmail.com>
2025-02-25 21:37:53 +0700

    Update RelayCommand

---
+++ string-matching-algorithm/Commands/RelayCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using System.Windows.Input;
+
+namespace string_matching_algorithm.Commands;
+public class RelayCommand<T> : ICommand {
+    private readonly Predicate<T> _canExecute;
+    private readonly Action<T> _execute;
+
+    public RelayCommand(Action<T> execute, Predicate<T>? canExecute = null) {
+        if (canExecute == null)
+            _canExecute = obj => true;
+        else
+            _canExecute = canExecute;
+
+        _execute = execute ?? throw new InvalidExpressionException();
+    }
+
+    public bool CanExecute(object? parameter) => _canExecute((T?)parameter);
+
+    public void Execute(object? parameter) => _execute((T?)parameter);
+
+    public event EventHandler? CanExecuteChanged {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+}
--- string-matching-algorithm/ViewModels/KMPViewModel.cs
+++ string-matching-algorithm/ViewModels/KMPViewModel.cs
@@ -1,20 +1,47 @@
 ﻿using NavigationMVVM.ViewModels;
 using string_matching_algorithm.Commands;
 using string_matching_algorithm.Stores;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
-namespace string_matching_algorithm.ViewModels; 
+namespace string_matching_algorithm.ViewModels;
 public class KMPViewModel : ViewModelBase {
-    public ICommand NavigateAlgorithmCommand { get; set; }
-
-    public KMPViewModel(NavigationStore navigationStore) {
-        NavigateAlgorithmCommand = new NavigateCommand<AlgorithmViewModel>(navigationStore, () => new AlgorithmViewModel(navigationStore));
-
+    //param goc luu tru du lieu
+    private string _nameTemp;
+    //param su dung thuc te
+    public string NameTemp {
+        get => _nameTemp;
+        set {
+            _nameTemp = value;
+            OnPropertyChanged();
+        }
     }
 
+    public ICommand NavigateAlgorithmCommand { get; set; }
+    //command
+    public ICommand Click1Command { get; set; }
+    //trigger
+    public ICommand Click2Command { get; set; }
+    public KMPViewModel(NavigationStore navigationStore) {
+        NavigateAlgorithmCommand = new NavigateCommand<AlgorithmViewModel>(navigationStore, () => new AlgorithmViewModel(navigationStore));
+        
+        //param binding 
+        NameTemp = "honguyen tai loi";
+        //function binding tu command 
+        Click1Command = new RelayCommand<object>(Click1);
+        //object la mot doi tuong can truyen vao 
+        //truyen vao mot ham Click va thuc hien tai ham click
+
+        //trigger
+        Click2Command = new RelayCommand<object>(Click2);
+
+    }
+    public void Click1(object sender) {
+        //code command
+        MessageBox.Show("Command da duoc thuc hien");
+    }
+    public void Click2(object sender) {
+        //code trigger
+        MessageBox.Show("Toi nhan duoc mot trigger");
+    }
 }
