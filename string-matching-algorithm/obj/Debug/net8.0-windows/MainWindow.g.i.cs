2e1ce49bc020e0704abe6dcf203458301a2b71b0
Superkid <kevinchristensen76923@gmail.com>
2025-02-27 23:31:56 +0700

    boyer moore + updated visualization

--- string-matching-algorithm/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ string-matching-algorithm/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -55,7 +55,7 @@ namespace string_matching_algorithm {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/string-matching-algorithm;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/string-matching-algorithm;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- string-matching-algorithm/obj/Debug/net8.0-windows/Views/GuideView.g.i.cs
+++ string-matching-algorithm/obj/Debug/net8.0-windows/Views/GuideView.g.i.cs
@@ -53,7 +53,7 @@ namespace string_matching_algorithm.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/string-matching-algorithm;V1.0.0.0;component/views/guideview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/string-matching-algorithm;component/views/guideview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\GuideView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
