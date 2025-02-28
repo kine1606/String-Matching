226d3676e9ac86b59e210afc5b4508300e41cecd
kine1606 <ntkien160605@gmail.com>
2025-02-28 23:48:14 +0700

    Bro what is this

--- string-matching-algorithm/obj/Debug/net8.0-windows/string-matching-algorithm.AssemblyInfo.cs
+++ string-matching-algorithm/obj/Debug/net8.0-windows/string-matching-algorithm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("string-matching-algorithm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+403f7c41118f93699b2ee6645fc27d03080965f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6275c1a2b0d4b8e874a0270425630e56bcc5d63d")]
 [assembly: System.Reflection.AssemblyProductAttribute("string-matching-algorithm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("string-matching-algorithm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
